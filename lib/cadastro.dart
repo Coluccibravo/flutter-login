c10bc3084396329d2ed85917d8c3ff00b5610c91
Coluccibravo <cl203231@g.unicamp.br>
2025-04-14 11:39:47 -0300

    ola

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -20,44 +20,62 @@ class _LoginState extends State<Login> {
         backgroundColor: Colors.lightGreen,
       ),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 70),
-            Icon(Icons.lock_person_outlined, size: 140),
-            SizedBox(height: 70),
-            Text(
-              "Login!",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 70),
-            Container(
-              padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-              child: Column(
-                children: [
-                  
-                  TextField(
-                    controller: _controller1,
-                    decoration: InputDecoration(
-                      label: Text("Nome do Usuario:"),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+        child: Center(
+          child: Form(
+            child: Column(
+              children: [
+                SizedBox(height: 70),
+                Icon(Icons.lock_person_outlined, size: 140),
+                SizedBox(height: 70),
+                Text(
+                  "Login!",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 70),
+                Container(
+                  padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _controller1,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return ("valor não pode ser nulo!");
+                          }
+                        },
+                        decoration: InputDecoration(
+                          label: Text("Nome do Usuario:"),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  SizedBox(height: 70),
-                  TextField(
-                    controller: _controller2,
-                    decoration: InputDecoration(
-                      label: Text("Senha do Usuario:"),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                      SizedBox(height: 70),
+                      TextFormField(
+                        controller: _controller2,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return ("valor não pode ser nulo!");
+                          } else {
+                            if (int.parse(value) < 10) {
+                              return ("valor tem que ser maior que zero!");
+                            }
+                          }
+                        },
+                        decoration: InputDecoration(
+                          label: Text("Senha do Usuario:"),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
